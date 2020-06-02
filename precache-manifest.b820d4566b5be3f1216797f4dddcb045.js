a486c3fdcb88fb584bbef7b49c6915772b051065
Joana Alves <joanasilverioferreira@gmail.com>
2020-06-02 15:49:34 +0100

    Updates

---
+++ precache-manifest.b820d4566b5be3f1216797f4dddcb045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1283305cb0cde56d2104606e77bc70",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5b2cc2205c9f81e6941c",
+    "url": "/weather-app/static/css/main.fe21079a.chunk.css"
+  },
+  {
+    "revision": "745f5f0f47e61d378bb4",
+    "url": "/weather-app/static/js/2.5e94cde2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.5e94cde2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2cc2205c9f81e6941c",
+    "url": "/weather-app/static/js/main.3787543a.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  }
+]);
